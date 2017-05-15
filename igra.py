212baf5f80fd664cee4f9e3cfd238b1a42cad79e
Marcel Spehonja <spehonja.marcel@gmail.com>
2017-05-15 13:51:40 +0200

    zacetek projekta

---
+++ igra.py
@@ -0,0 +1,17 @@
+
+class Plosca:
+    def __init__(self):
+        velikost_plosce
+
+        stevilo_polj
+
+        pozicije_polj
+
+
+class Figura:
+    def __init__(self, ):
+        zacetna_pozicija
+
+
+
+class Kocka
